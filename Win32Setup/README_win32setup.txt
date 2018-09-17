bdeba81dee8ccc05c8a88482c40cf71c2593a509
Gianluca <gianluca@DebianProgrammazione.org>
2018-09-17 13:09:22 +0200

    1.0.0 commit

---
+++ Win32Setup/README_win32setup.txt
@@ -0,0 +1,8 @@
+
+DESCRIPTION:
+    
+These files are useful for replacing all * .py on vdms_PROCESS when creating a 
+Videomass.exe with current setup.py and py2exe. It differs only for some lines 
+of code added to subprocess.popen, necessary to prevent the dos window from 
+appearing during processes with Videomass.exe package only.
+
---
+++ vdms_IO/make_filelog.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+
+#########################################################
+# File Name: make_filelog.py
+# Porpose: Module to generate log files durin jobs start
+# Author: Gianluca Pernigoto <jeanlucperni@gmail.com>
+# Copyright: (c) 2018/19 Gianluca Pernigoto <jeanlucperni@gmail.com>
+# license: GPL3
+
+# This file is part of Videomass.
+
+#    Videomass is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Videomass is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Videomass.  If not, see <http://www.gnu.org/licenses/>.
+
+# Rev (09) 16 july 2018
+#########################################################
+
+import time
+import os
+
+DIRNAME = os.path.expanduser('~') # /home/user
+
+def write_log(logname):
+    """
+    During the process, it write log about what the program does,
+    command output and including errors.
+    The log is stored in ~/user/.videomass by default.
+
+    - logname è il nome del pannello da cui è stato composto il comando
+    """
+    current_date =  time.strftime("%c") # date/time
+
+    with open("%s/.videomass/%s" % (DIRNAME, logname),"w") as log:
+        log.write("""[PYTHON] CURRENT DATE/TIME:
+%s\n
+-----------------------------------------
+[VIDEOMASS] INFO FOR USERS: 
+-----------------------------------------
+If you want to save the log file in other places of
+your drives, set it by videomass preferences dialog.
+-----------------------------------------
+Videomass default ffmpeg loglevel fixed to 'error'.
+See ~/.videomass/videomass.conf for more details.
+-----------------------------------------
+
+[VIDEOMASS] COMMAND LINE:
+
+""" % (current_date))
