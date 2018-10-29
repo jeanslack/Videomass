b79e0371a82222f277f682b72bd4c39ea62fecdb
jeanslack <gianluca@debianMini.org>
2018-10-29 22:56:09 +0100

    update 1.2.0

--- vdms_DIALOGS/dialog_tools.py
+++ vdms_DIALOGS/dialog_tools.py
@@ -75,7 +75,7 @@ class Cut_Range(wx.Dialog):
         self.start_second_ctrl = wx.SpinCtrl(self, wx.ID_ANY,"%s" % (
                self.init_seconds), min=0, max=59, style=wx.TE_PROCESS_ENTER)
         sizer_1_staticbox = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, (
-                        "Start time position (hours:min:sec)")), wx.VERTICAL)
+                        "Seeking (time position) [hh,mm,ss]")), wx.VERTICAL)
         self.stop_hour_ctrl = wx.SpinCtrl(self, wx.ID_ANY, "%s" % (
                    self.cut_hour), min=0, max=23, style=wx.TE_PROCESS_ENTER)
         lab3 = wx.StaticText(self, wx.ID_ANY, (":"))
@@ -87,14 +87,14 @@ class Cut_Range(wx.Dialog):
         self.stop_second_ctrl = wx.SpinCtrl(self, wx.ID_ANY, "%s" % (
                 self.cut_seconds), min=0, max=59, style=wx.TE_PROCESS_ENTER)
         sizer_2_staticbox = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, (
-                        "Time progress duration (hours:min:sec)")), wx.VERTICAL)
+                        "Cut (time duration) [hh,mm,ss]")), wx.VERTICAL)
         btn_help = wx.Button(self, wx.ID_HELP, "", size=(-1, -1))
         btn_close = wx.Button(self, wx.ID_CANCEL, "")
         btn_ok = wx.Button(self, wx.ID_OK, "")
         btn_reset = wx.Button(self, wx.ID_CLEAR, "")
 
         #----------------------Properties ----------------------#
-        self.SetTitle('Set Time Sequences - Videomass2')
+        self.SetTitle('Duration - Videomass2')
         self.start_hour_ctrl.SetMinSize((100,-1 ))
         self.start_minute_ctrl.SetMinSize((100, -1))
         self.start_second_ctrl.SetMinSize((100, -1))
