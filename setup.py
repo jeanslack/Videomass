1ccce31fa6bb6e8caef62008429d109bfc99aa91
Gianluca Pernigotto <jeanlucperni@gmail.com>
2020-07-15 21:12:27 +0200

    added debianize folder

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-#########################################################
 # Name: setup.py
 # Porpose: script to setup Videomass.
 # Compatibility: Python3
@@ -9,7 +5,7 @@
 # Writer: Gianluca Pernigoto <jeanlucperni@gmail.com>
 # Copyright: (c) 2014-2020 Gianluca Pernigoto <jeanlucperni@gmail.com>
 # license: GPL3
-# Rev: June.14.2020 *PEP8 compatible*
+# Rev: July.15.2020 *PEP8 compatible*
 #########################################################
 
 # This file is part of Videomass.
@@ -35,63 +31,31 @@ import sys
 from videomass3.vdms_sys.msg_info import current_release
 from videomass3.vdms_sys.msg_info import descriptions_release
 
-# ---- current work directory path ----#
-PWD = os.getcwd()
-
-# ---- Get info data ----#
-cr = current_release()
-RLS_NAME = cr[0]  # release name first letter is Uppercase
-PRG_NAME = cr[1]
-VERSION = cr[2]
-RELEASE = cr[3]
-COPYRIGHT = cr[4]
-WEBSITE = cr[5]
-AUTHOR = cr[6]
-EMAIL = cr[7]
-COMMENT = cr[8]
-
-dr = descriptions_release()
-LICENSE = dr[2]  # short license
-DESCRIPTION = dr[0]
-LONG_DESCRIPTION = dr[1]
-
-# ---- categorize with ----#
-CLASSIFIERS = [
-        'Environment :: MacOS X :: Cocoa',
-        'Environment :: Win32 (MS Windows)',
-        'Environment :: X11 Applications :: GTK',
-        'Development Status :: 4 - Beta',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows :: Windows 7',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'Operating System :: POSIX',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Natural Language :: Italian',
-        'Programming Language :: Python :: 3.7',
-        "Programming Language :: Python :: 3.8",
-        'Topic :: Multimedia :: Video :: Conversion',
-        'Topic :: Multimedia :: Sound/Audio :: Conversion',
-        'Topic :: Utilities',
-                ]
-# ---------------------------------------------------------------------#
-
-
-def glob_files(pattern):
-    """
-    Useful function for globbing that iterate on a pattern marked
-    with wildcard and put it in a objects list
-    """
-    return [f for f in glob(pattern) if os.path.isfile(f)]
-# ---------------------------------------------------------------------#
-
 
 def source_build():
     """
     Source/Build distributions
 
     """
+    PWD = os.getcwd()
+
+    # Get info data
+    cr = current_release()
+    RLS_NAME = cr[0]  # release name first letter is Uppercase
+    PRG_NAME = cr[1]
+    VERSION = cr[2]
+    RELEASE = cr[3]
+    COPYRIGHT = cr[4]
+    WEBSITE = cr[5]
+    AUTHOR = cr[6]
+    EMAIL = cr[7]
+    COMMENT = cr[8]
+
+    dr = descriptions_release()
+    LICENSE = dr[2]  # short license
+    DESCRIPTION = dr[0]
+    LONG_DESCRIPTION = dr[1]
+
     if 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
 
         INSTALL_REQUIRES = ["wxpython>=4.0.3; platform_system=='Windows' or "
@@ -99,14 +63,16 @@ def source_build():
                             "PyPubSub>=4.0.3",
                             "youtube_dl>=2020.1.1",
                             ]
+        SETUP_REQUIRES = ["setuptools>=47.1.1", "wheel>=0.34.2","twine>=3.1.1"]
         long_description=open('README.md').read()
         long_description_content_type='text/markdown'
 
     else:  # if use stdeb to make .deb or deb pkg src, include wxpython.
         INSTALL_REQUIRES = ["wxpython>=4.0.3",
                             "PyPubSub>=4.0.3",
-                            "youtube_dl>=2019.01.17",
+                            "youtube_dl",
                             ]
+        SETUP_REQUIRES = []
         long_description=LONG_DESCRIPTION
         long_description_content_type='text'
 
@@ -140,15 +106,30 @@ def source_build():
                                 },
           include_package_data=True,
           zip_safe=False,
-          python_requires=">=3.7, <4",
+          python_requires=">=3.6.9, <4",
           install_requires=INSTALL_REQUIRES,
-          setup_requires=["setuptools>=47.1.1",
-                          "wheel>=0.34.2",
-                          "twine>=3.1.1"
-                          ],
+          setup_requires=SETUP_REQUIRES,
           entry_points={'gui_scripts':
                         ['videomass = videomass3.Videomass3:main']},
-          classifiers=CLASSIFIERS,
+          classifiers=[
+            'Environment :: MacOS X :: Cocoa',
+            'Environment :: Win32 (MS Windows)',
+            'Environment :: X11 Applications :: GTK',
+            'Development Status :: 4 - Beta',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows :: Windows 7',
+            'Operating System :: Microsoft :: Windows :: Windows 10',
+            'Operating System :: POSIX',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Natural Language :: English',
+            'Natural Language :: Italian',
+            'Programming Language :: Python :: 3.7',
+            "Programming Language :: Python :: 3.8",
+            'Topic :: Multimedia :: Video :: Conversion',
+            'Topic :: Multimedia :: Sound/Audio :: Conversion',
+            'Topic :: Utilities',
+            ],
           )
 # ---------------------------------------------------------------------#
 
