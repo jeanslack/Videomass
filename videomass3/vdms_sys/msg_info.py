2e6c19c9db14422b26017ba07b1612a1b16b4bf3
Gianluca Pernigotto <jeanlucperni@gmail.com>
2021-08-29 12:18:00 +0200

    fix changelog

--- videomass3/vdms_sys/msg_info.py
+++ videomass3/vdms_sys/msg_info.py
@@ -40,7 +40,7 @@ def current_release():
     release_name = 'Videomass'
     program_name = 'videomass'
     version = '3.4.4'
-    release = 'Aug 12 2021'
+    release = 'Aug 29 2021'
     copyr = '2013-2021'
     website = 'http://jeanslack.github.io/Videomass/'
     author = ('Gianluca Pernigotto', '(aka jeanslack)')
