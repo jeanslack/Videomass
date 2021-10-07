e3b9295225f41a911b79cb687d45290a73ecd8ad
Gianluca Pernigotto <jeanlucperni@gmail.com>
2021-10-07 12:54:23 +0200

    update 3.5.0 fixed ydl selectors

--- videomass/vdms_sys/msg_info.py
+++ videomass/vdms_sys/msg_info.py
@@ -39,8 +39,8 @@ def current_release():
     """
     release_name = 'Videomass'
     program_name = 'videomass'
-    version = '3.4.8'
-    release = 'Oct 04 2021'
+    version = '3.5.0'
+    release = 'Oct 07 2021'
     copyr = '2013-2021'
     website = 'http://jeanslack.github.io/Videomass/'
     author = ('Gianluca Pernigotto', '(aka jeanslack)')
