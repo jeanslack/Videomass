b191180f966c0fa11c7a04e683a2d266d27574a2
Gianluca Pernigotto <jeanlucperni@gmail.com>
2024-03-29 00:21:36 +0100

    make release

--- videomass/vdms_sys/msg_info.py
+++ videomass/vdms_sys/msg_info.py
@@ -37,7 +37,7 @@ def current_release():
     release_name = 'Videomass'
     program_name = 'videomass'
     version = '5.0.10'
-    release = 'not released'
+    release = 'released'
     copyr = '2013-2024'
     website = 'http://jeanslack.github.io/Videomass/'
     author = ('Gianluca Pernigotto', '(aka jeanslack)')
