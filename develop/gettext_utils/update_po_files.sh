c3300b81150648c9bba7fec156a2f6105ef41d09
Gianluca Pernigotto <jeanlucperni@gmail.com>
2024-07-05 23:01:33 +0200

    fix gettext utils

--- develop/gettext_utils/update_po_files.sh
+++ develop/gettext_utils/update_po_files.sh
@@ -3,14 +3,14 @@
 # Author: Gianluca Pernigotto <jeanlucperni@gmail.com>
 # Copyleft - 2024 Gianluca Pernigotto <jeanlucperni@gmail.com>
 # license: GPL3
-# Rev: July.04.2024
+# Rev: July.05.2024
 #
 # PORPOSE: Updates all translation strings of videomass.po files contained in
 #          a given directory (usually "videomass/data/locale"). It accept
-#          absolute or relative path names. If the optional `compile=true` 
-#          argument is given, it also compiles the MO bin files .
+#          absolute or relative path names. If `compile=true` argument is given
+#          it compile MO bin
 #
-# USAGE: ~$ update_po_files.sh "/path/to/locale/directory" [compile=true]
+# USAGE: ~$ update_po_files.sh '/path/to/locale/directory' [compile=true]
 
 TARGET=$1
 POTFILE="$TARGET/videomass.pot"
@@ -41,7 +41,7 @@ fi
 for langdirs in $(ls -d ${TARGET}/*/)
 do
     PO="${langdirs}LC_MESSAGES/videomass.po"
-    msgmerge --update $PO $POTFILE
+    msgmerge --update --no-fuzzy-matching --width=400 --no-wrap $PO $POTFILE
     if [ "${COMPILE_MO_BIN}" = 'compile=true' ]; then
         echo "Now compile videomass.mo file because compile=true"
         msgfmt -c "${PO}" -o "${langdirs}LC_MESSAGES/videomass.mo"
