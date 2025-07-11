8ac70e14d80ffe10dee8eaf588ed7826819c003d
Gianluca Pernigotto <jeanlucperni@gmail.com>
2025-07-11 09:01:56 +0200

    Update msg catalogs

---
+++ develop/gettext_utils/purge_deleted_translation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Author: Gianluca Pernigotto <jeanlucperni@gmail.com>
+# Copyleft - 2025 Gianluca Pernigotto <jeanlucperni@gmail.com>
+# license: GPL3
+# Rev: July.10.2024
+#
+# PORPOSE: Delete obsolete translation messages on videomass.po files
+#          for each locale found on the TARGET directory.
+#
+# USAGE: ~$ cd /Videomass/source/directory
+#        ~$ develop/gettext_utils/update_MO_files.sh
+
+TARGET="videomass/data/locale"  # relative path to "locale" directory
+
+echo -e "\nTarget directory: \033[34m\e[1m'${TARGET}'\e[0m"
+
+if [ ! -d "${TARGET}" ]; then
+    echo -e "\033[31mERROR:\e[0m Directory does not exist: '${TARGET}'"
+    exit 1
+fi
+
+for langdirs in $(ls -d ${TARGET}/*/)
+do
+    PO="${langdirs}LC_MESSAGES/videomass.po"
+    #msgfmt --check "${PO}" --output-file="${langdirs}LC_MESSAGES/videomass.mo"
+    msgattrib --output-file="${PO}" --no-obsolete "${PO}"
+    if [[ $? -ne 0 ]]; then
+        echo -e '\e[1m\033[31mFailed!\e[0m'
+        exit $?
+    fi
+    echo "INFO: Deleting obsolete: '${langdirs}LC_MESSAGES/videomass.mo' ...OK"
+done
+
+echo -e "\n\e[1mSuccessfully updated.\e[0m"
+echo -e "\033[32mDone!\e[0m"
