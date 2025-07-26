ef090389bb0c404c62c7e5ba2fee9327e7905f16
Gianluca Pernigotto <jeanlucperni@gmail.com>
2025-07-26 15:11:07 +0200

    Update translation catalogs

--- videomass/vdms_panels/filedrop.py
+++ videomass/vdms_panels/filedrop.py
@@ -6,7 +6,7 @@ Compatibility: Python3, wxPython Phoenix
 Author: Gianluca Pernigotto <jeanlucperni@gmail.com>
 Copyleft - 2025 Gianluca Pernigotto <jeanlucperni@gmail.com>
 license: GPL3
-Rev: Feb.18.2024
+Rev: July.26.2025
 Code checker: flake8, pylint
 
 This file is part of Videomass.
